c05c743a10a2d652f478bca79acc7e9695bd00bc
IIchukissII <133657307+IIchukissII@users.noreply.github.com>
2024-01-19 19:44:19 +0100

    Delete dfs.py

--- dfs.py
+++
@@ -1,9 +0,0 @@
-def dfs_recursive(graph, vertex, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(vertex)
-    print(vertex, end=" ")  # Відвідуємо вершину
-    for neighbor in graph[vertex]:
-        if neighbor not in visited:
-            dfs_recursive(graph, neighbor, visited)
- 
